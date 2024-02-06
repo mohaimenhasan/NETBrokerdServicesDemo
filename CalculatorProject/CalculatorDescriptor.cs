bdf811bd3ac45fb1d3c7913c256409cc0d0dc84b
Mohaimen Khan <mohaimenkhan@microsoft.com>
2024-02-06 11:33:42 -0800

    added service descriptor

---
+++ CalculatorProject/CalculatorDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ServiceHub.Framework;
+using Nerdbank.Streams;
+
+namespace CalculatorProject
+{
+    public static class CalculatorDescriptor
+    {
+        public static readonly ServiceRpcDescriptor CalculatorService = new ServiceJsonRpcDescriptor(
+            new ServiceMoniker("MohaimenCoolCompany.CoolMath.Calculator", new Version(1, 0)),
+            clientInterface: null,
+            ServiceJsonRpcDescriptor.Formatters.MessagePack,
+            ServiceJsonRpcDescriptor.MessageDelimiters.BigEndianInt32LengthHeader,
+            new MultiplexingStream.Options { ProtocolMajorVersion = 3 })
+            .WithExceptionStrategy(StreamJsonRpc.ExceptionProcessing.ISerializable);
+    }
+}
